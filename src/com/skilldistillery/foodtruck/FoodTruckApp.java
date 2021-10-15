d5a6ad5c76538aaa8d439a0ab27302b29e086bab
CJ Harris <Carroll.Harris2@gmail.com>
2021-10-15 19:22:21 -0400

    initial set-up

---
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.foodtruck;
+
+public class FoodTruckApp {
+	//only place with scanner, menu
+	//use food trucks methods to access data. getters setters
+	//list only the food trucks that have been inputted. dont show null
+	//average rating done with .length
+	//2 loops, one for user input data, one for menu
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//
+	}
+
+}
